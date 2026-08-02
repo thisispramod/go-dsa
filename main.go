8d4fa7b31f914a47d2f859c3231a88d6b27a95cf
thisispramod <itspramod19@gmail.com>
2026-08-02 22:48:11 +0530

    formatting

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	/* resA, resB := SwapXor(10, 20)
-	fmt.Println("a swap to b:", resA, resB) */
+	/*
+		resA, resB := SwapXor(10, 20)
+		fmt.Println("a swap to b:", resA, resB)
+	*/
 
 	/*
-		fmt.Println("count of string is ", UniqueFirst("swiss")) */
-	arr := []int{2, 7, 11, 15}
-	target := 9
-	fmt.Println("Two sum problem indices", TwoSum(arr, target))
+		fmt.Println("count of string is ", UniqueFirst("swiss"))
+	*/
+
+	/*
+		arr := []int{2, 7, 11, 15}
+		target := 9
+		fmt.Println("Two sum problem indices", TwoSum(arr, target))
+	*/
 
 }
